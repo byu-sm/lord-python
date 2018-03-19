595d19a02032e76b0e9c919eb69780e5a4f0a156
Brennan Ackerman <brennan.ackerman@ubnt.com>
2018-03-19 08:24:30 -0600

    adding gui

--- main.py
+++ main.py
@@ -1,30 +1,45 @@
-import Lord/mscl
+import mscl
 import config
+
+from tkinter import *
+
+def displayNodes(nodes):
+    nodeDisplay = Tk()
+    Label(nodeDisplay, text=nodes).grid(row=0)
+    e1 = Entry(nodeDisplay)
+    e1.grid(row=0, column=0)
+
+    Button(nodeDisplay(), text='Add Node', command=add_node).grid(row=1, column=0, sticky=W, pady=4)
+    Button(nodeDisplay(), text='Quit', command=nodeDisplay.quit).grid(row=1, column=1, sticky=W, pady=4)
+
+def displayPrompt(bs,nodes):
+    if bs == null:
+        promptStation()
+    else:
+        displayNodes(nodes)
 
 def main():
     print("Intializing...")
 
-    # Use this space for GUI code and gathering information about sensors to connect
-    
-    stations = []
     networks = []
     nodes_master = []
-    for bs in config.basestations:
-        conn = mscl.Connection.Serial(bs["com_port"], bs["baud_rate"])
-        station = mscl.BaseStation(conn)
-        stations.add(station)
-        network = mscl.SyncSamplingNetwork(station)
+    
+    conn = mscl.Connection.Serial(config.basestation["com_port"], basestation["baud_rate"])
+    bs = mscl.BaseStation(conn)
+    network = mscl.SyncSamplingNetwork(bs)
 
-        nodes = []
-        for n in config.basestations["nodes"]:
-            node = mscl.WirelessNode(n["address"], station)
-            nodes.add(node)
-            network.addNode(node)
-        nodes_master.add(nodes)
+    nodes = []
+    for n in config.basestations["nodes"]:
+        node = mscl.WirelessNode(n["address"], bs)
+        nodes.add(node)
+        network.addNode(node)
+    nodes_master.add(nodes)
 
-        # Read config (bs["config"])
-        network.applyConfiguration()
-        networks.add(network)
+    displayPrompt(bs,nodes)
+
+    # Read config (bs["config"])
+    network.applyConfiguration()
+    networks.add(network)
 
 if __name__ == "__main__":
     main()
